9f90023cc6d95b17ce4deb5bd9b93c8b7acfd662
Isha Agarwal <ishaagarwal772000@gmail.com>
2022-04-29 23:04:45 +0530

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 function App() {
   return (
     <>
-      <h1>Welcome to ProShop</h1> 
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to ProShop</h1> 
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './bootstrap.min (1).css'
 import reportWebVitals from './reportWebVitals';
-
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

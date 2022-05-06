c525e9f0c13fdccf72e4cf244a99d558c3ae216f
Isha Agarwal <ishaagarwal772000@gmail.com>
2022-05-07 00:16:29 +0530

    'router'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 function App() {
   return (
-    <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-        <HomeScreen /> 
-      </Container>
-    </main>
-    <Footer />
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/product/:id' element={<ProductScreen />} exact />
+            </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 

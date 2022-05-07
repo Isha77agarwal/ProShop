1b3059b94c5d3946dac7aabc271f411a24aa2b30
Isha Agarwal <ishaagarwal772000@gmail.com>
2022-05-07 18:10:33 +0530

    database model

---
+++ Backend/Data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+
+const users=[
+    {
+        name:'Admin',
+        email:'admin@gmail.com',
+        password:bcrypt.hashSync('123456', 10),
+        isAdmin:true
+    },
+    {
+        name:'Yuvi',
+        email:'yuvi@gmail.com',
+        password:bcrypt.hashSync('123456', 10)
+    },
+    {
+        name:'Isha',
+        email:'isha7701@gmail.com',
+        password:bcrypt.hashSync('123456', 10)
+    }
+]
+
+export default users
---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import products from './Data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+const app = express()
+
+dotenv.config()
+
+connectDB()
+
+app.get('/', (req,res) => {
+    res.send("API is running....")
+})
+
+app.get('/api/products', (req,res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log("server runnning....".yellow.bold))
